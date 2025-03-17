558afb113dd175c1cc20ba32356b18f89a4ad0e4
Karen Yao <karenyaooo@gmail.com>
2025-03-16 20:14:50 -0700

    Creating client file

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket 
+
+data = ""
+compareScores = ""
+score = 0
+winner = -1
+
+def send_score_to_server(scored):
+        
+def getResult():
+    return data
+
+def getWinter():
+    return winner 
+
+def main():
+    print("this is client yippee")
+
+if __name__ == "main":
+    main()
