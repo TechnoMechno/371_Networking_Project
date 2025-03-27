eddd75066bc0cf3b4ac298dc81cf2ee51659dc4f
Karen Yao <karenyaooo@gmail.com>
2025-03-26 17:06:51 -0700

    created GameClient

---
+++ GameClient.py
@@ -0,0 +1,65 @@
+from cookie import Cookie
+import socket 
+import threading
+import pygame
+
+# PORT and IP Address 
+PORT = 5555 
+ip_address = "localhost"
+
+in_game = False
+player_id = -1 # The player ID assigned by the server.
+cookie_id = -1 # The cookie ID picked up by player
+mouse_x = pygame.mouse.get_pos()[0]
+mouse_y = pygame.mouse.get_pos()[1]
+is_dragging = False
+
+update_message = {
+    'player_id': player_id,
+    'mouse_x': mouse_x,
+    'mouse_y': mouse_y,
+    'cookie_id': cookie_id,
+    'is_dragging': is_dragging
+}
+
+class GameClient:
+    def __init__(self, player_id):
+        self.player_id = player_id
+
+    def join_game(self):
+        """Join the game lobby."""
+        # TODO: implement client to server 
+        pass
+
+    def receive_from_server():
+        """Receive messages from the server."""
+        while True:
+            data = client_socket.recv(1024)
+
+
+    def if_dragging(self):
+        """Check if the player is dragging the cookie."""
+        global is_dragging 
+        if (pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pos() == (cookie)):
+            is_dragging = True
+        else:
+            is_dragging = False
+        pass
+
+    def get_mouse_location():
+        """Get coordinates from website."""
+        global mouse_x, mouse_y
+        mouse_x = pygame.mouse.get_pos()[0]
+        mouse_y = pygame.mouse.get_pos()[1]
+
+    def tick():
+        """Update the game state."""
+
+        pass
+        
+def main():
+    server_address = (ip_address, PORT) 
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.connect(server_address)
+
+    
